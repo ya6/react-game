7a66a435868d3513bbd87ff3eacf88afe26b36a3
YA <vector_cp@mail.ru>
2021-02-22 20:57:37 +0300

    feat: set basic env

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Component } from 'react';
+
+class  App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
