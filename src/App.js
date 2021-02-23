4baad51b797fec21056b54c13049622fa3717b53
YA <vector_cp@mail.ru>
2021-02-23 23:52:21 +0300

    feat: add basic fill field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import { Component } from "react";
 import Field from "./components/field";
-import _random from './helpers/_random'
-
+import _random from "./helpers/_random";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      fieldSize: 10
-      ,
+      fieldSize: 10,
       field: [],
     };
   }
@@ -21,21 +19,74 @@ class App extends Component {
 
     for (let y = 0; y < fieldSize; y++) {
       _arr[y] = new Array(fieldSize);
+      _arr[y].fill(0);
     }
 
+    //set bombs
     for (let b = 0; b < fieldSize; b++) {
-      _arr[_random(0, this.state.fieldSize-1)][_random(0, this.state.fieldSize-1)] = -1
+      _arr[_random(0, this.state.fieldSize - 1)][
+        _random(0, this.state.fieldSize - 1)
+      ] = -1;
     }
 
-   // console.log(_arr);
-    this.setState( { field: _arr});
+    //todo set other block
+
+    for (let y = 0; y < fieldSize; y++) {
+      for (let x = 0; x < fieldSize; x++) {
+        if (_arr[y][x] !== -1) {
+
+          if (y - 1 >= 0 && x - 1 >= 0) {
+            _arr[y][x] += +(_arr[y - 1][x - 1] === -1);
+          }
+
+          if (y - 1 >= 0) {
+            _arr[y][x] += +(_arr[y - 1][x] === -1);
+          }
+
+          if (y - 1 >= 0 && x + 1 < fieldSize) {
+            _arr[y][x] += +(_arr[y - 1][x + 1] === -1);
+          }
+
+          if (x + 1 < fieldSize) {
+            _arr[y][x] += +(_arr[y][x + 1] === -1);
+          }
+          
+          if (y + 1 < fieldSize && x + 1 < fieldSize) {
+            _arr[y][x] += +(_arr[y + 1][x + 1] === -1);
+          }
+
+          if (y + 1 <fieldSize) {
+            _arr[y][x] += +(_arr[y + 1][x] === -1);
+          }
+
+          if (y + 1 <fieldSize && x-1 >= 0) {
+            _arr[y][x] += +(_arr[y + 1][x-1] === -1);
+          }
+
+          if (x-1 >= 0) {
+            _arr[y][x] += +(_arr[y][x-1] === -1);
+          }
+
+          //_arr[y-1][x-1]
+          // _arr[y-1][x]
+          // _arr[y-1][x+1]
+          // _arr[y][x+1]
+          // _arr[y+1][x+1]
+          // _arr[y+1][x]
+          // _arr[y+1][x-1]
+
+          // _arr[y][x-1]
+        }
+      }
+    }
+
+    this.setState({ field: _arr });
   }
 
   render() {
     return (
       <div className="App">
-
-        <Field  field = {this.state.field}/>
+        <Field field={this.state.field} />
       </div>
     );
   }
--- src/components/field-block/field-block.component.jsx
+++ src/components/field-block/field-block.component.jsx
@@ -1,7 +1,6 @@
 import "./field-block.style.css";
 
 const FieldBlock = ({ block }) => {
-  console.log('block', block);
   return (
       <div className = "field-block">
           {block}
