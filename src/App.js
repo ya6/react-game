a8044b4532c8077f372c78522f301d857d6e4994
YA <vector_cp@mail.ru>
2021-02-28 21:50:12 +0300

    feact: add basic interface: add rerender new size of map

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import back_url from "./assets/img/dig_bel_2+.png";
 
 class App extends Component {
   state = {
-    fieldWidth: 20,
-    fieldHeight: 16,
+    fieldWidth: 15,
+    fieldHeight: 12,
+    field_size_factor: 3,
     size: 50,
     spots: 0,
     field: [],
@@ -23,6 +24,20 @@ class App extends Component {
   };
 
   removed_canvasRef = React.createRef();
+
+
+  fieldSizeHandler = (e) => {
+    console.log('fieldSizeHandler ', e.target.value);
+
+    const _field_size_factor =   e.target.value;
+
+    const _fieldWidth = _field_size_factor * 5;
+    const _fieldHeight = _field_size_factor * 4;
+
+    this.setState( { fieldWidth: _fieldWidth,
+      fieldHeight: _fieldHeight,
+      field_size_factor: _field_size_factor } );
+  }
 
   componentDidMount() {
     console.log("App componentDidMount");
@@ -39,187 +54,10 @@ class App extends Component {
    
 
     const _imageRatio = imageWidth / (fieldWidth * size);
-    const _spots = Math.floor(fieldWidth * fieldHeight * 0.1);
-    console.log(_spots);
+   // const _spots = Math.floor(fieldWidth * fieldHeight * 0.1);
+   
 
-    //get arr
-    const _arr = [];
-
-    const canvas = this.removed_canvasRef.current;
-    const context = canvas.getContext("2d");
-    canvas.width = fieldWidth * size;
-    canvas.height = fieldHeight * size;
-
-    const image = new Image();
-    image.src = back_url;
-    image.addEventListener("load", () => {
-      context.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-      // step inside
-      const compensation = 3;
-
-      for (let y = 0; y < fieldHeight; y++) {
-        _arr[y] = [];
-
-        for (let x = 0; x < fieldWidth; x++) {
-          if (
-            context.getImageData(
-              x * size + compensation,
-              y * size + compensation,
-              1,
-              1
-            ).data[0] > 0 ||
-            context.getImageData(
-              x * size + compensation,
-              y * size + size + compensation,
-              1,
-              1
-            ).data[0] > 0 ||
-            context.getImageData(
-              x * size + size - compensation,
-              y * size + compensation,
-              1,
-              1
-            ).data[0] > 0 ||
-            context.getImageData(
-              x * size + size - compensation,
-              y * size + size - compensation,
-              1,
-              1
-            ).data[0] > 0 ||
-            context.getImageData(
-              x * size + compensation,
-              y * size + size / 2 - compensation,
-              1,
-              1
-            ).data[0] > 0 ||
-            context.getImageData(
-              x * size + compensation,
-              y * size + size / 2 - compensation,
-              1,
-              1
-            ).data[0] > 0
-          ) {
-            _arr[y][x] = {
-              val: 0,
-              status: "hidden",
-              y: y,
-              x: x,
-              size: size,
-              left: x * size,
-              top: y * size,
-              med: false,
-              updated: 0,
-            };
-          } else {
-            _arr[y][x] = {
-              val: -2,
-              status: "hidden",
-              y: y,
-              x: x,
-              size: size,
-              left: x * size,
-              top: y * size,
-              med: false,
-              updated: 0,
-            };
-          }
-
-          //set num
-          context.font = 20 + "px Arial";
-          context.textAlign = "center";
-          context.textBaseline = "middle";
-          context.fillStyle = "gray";
-
-          context.fillText(
-            String(_arr[y][x].val),
-            x * size + 20,
-            y * size + 20
-          );
-
-          context.strokeStyle = "red";
-          context.strokeRect(
-            x * size + compensation,
-            y * size + compensation,
-            1,
-            1
-          );
-          context.strokeStyle = "green";
-          context.strokeRect(
-            x * size + compensation,
-            y * size + size - compensation,
-            1,
-            1
-          );
-          context.strokeStyle = "blue";
-          context.strokeRect(
-            x * size + size - compensation,
-            y * size + compensation,
-            1,
-            1
-          );
-          context.strokeStyle = "orange";
-          context.strokeRect(
-            x * size + size - compensation,
-            y * size + size - compensation,
-            1,
-            1
-          );
-        }
-      }
-
-      // set bombs
-      let b = 0;
-      console.log("spots", this.state.spots);
-      while (b < _spots) {
-        const _y = _random(0, fieldHeight - 1);
-        const _x = _random(0, fieldWidth - 1);
-
-        if (_arr[_y][_x].val !== -2 && _arr[_y][_x].val !== -1) {
-          b += 1;
-          _arr[_y][_x].val = -1;
-        }
-      }
-
-      // set other blocks
-      for (let y = 0; y < fieldHeight; y++) {
-        for (let x = 0; x < fieldWidth; x++) {
-          if (_arr[y][x].val !== -1 && _arr[y][x].val !== -2) {
-            if (y - 1 >= 0 && x - 1 >= 0) {
-              _arr[y][x].val += +(_arr[y - 1][x - 1].val === -1);
-            }
-
-            if (y - 1 >= 0) {
-              _arr[y][x].val += +(_arr[y - 1][x].val === -1);
-            }
-
-            if (y - 1 >= 0 && x + 1 < fieldHeight) {
-              _arr[y][x].val += +(_arr[y - 1][x + 1].val === -1);
-            }
-
-            if (x + 1 < fieldWidth) {
-              _arr[y][x].val += +(_arr[y][x + 1].val === -1);
-            }
-
-            if (y + 1 < fieldHeight && x + 1 < fieldWidth) {
-              _arr[y][x].val += +(_arr[y + 1][x + 1].val === -1);
-            }
-
-            if (y + 1 < fieldHeight) {
-              _arr[y][x].val += +(_arr[y + 1][x].val === -1);
-            }
-
-            if (y + 1 < fieldHeight && x - 1 >= 0) {
-              _arr[y][x].val += +(_arr[y + 1][x - 1].val === -1);
-            }
-
-            if (x - 1 >= 0) {
-              _arr[y][x].val += +(_arr[y][x - 1].val === -1);
-            }
-          }
-        }
-      }
-    });
+    this.setNewField();
 
     //set base state
     //
@@ -227,13 +65,212 @@ class App extends Component {
 
     this.setState(
       {
-        field: _arr,
+       
         imageRatio: _imageRatio,
-        spots: _spots,
+     
         screenWidth: _screenWidth,
         screenHeight: _screenHeight
       }
     );
+  }
+
+  setNewField = () => {
+     //get arr
+
+     const {fieldWidth, fieldHeight, size} = this.state;
+     const _spots = Math.floor(fieldWidth * fieldHeight * 0.1);
+     const _arr = [];
+
+     const canvas = this.removed_canvasRef.current;
+     const context = canvas.getContext("2d");
+     canvas.width = fieldWidth * size;
+     canvas.height = fieldHeight * size;
+ 
+     const image = new Image();
+     image.src = back_url;
+     image.addEventListener("load", () => {
+ 
+ 
+       context.drawImage(image, 0, 0, canvas.width, canvas.height);
+ 
+       // step inside
+       const compensation = 3;
+ 
+       for (let y = 0; y < fieldHeight; y++) {
+         _arr[y] = [];
+ 
+         for (let x = 0; x < fieldWidth; x++) {
+           if (
+             context.getImageData(
+               x * size + compensation,
+               y * size + compensation,
+               1,
+               1
+             ).data[0] > 0 ||
+             context.getImageData(
+               x * size + compensation,
+               y * size + size + compensation,
+               1,
+               1
+             ).data[0] > 0 ||
+             context.getImageData(
+               x * size + size - compensation,
+               y * size + compensation,
+               1,
+               1
+             ).data[0] > 0 ||
+             context.getImageData(
+               x * size + size - compensation,
+               y * size + size - compensation,
+               1,
+               1
+             ).data[0] > 0 ||
+             context.getImageData(
+               x * size + compensation,
+               y * size + size / 2 - compensation,
+               1,
+               1
+             ).data[0] > 0 ||
+             context.getImageData(
+               x * size + compensation,
+               y * size + size / 2 - compensation,
+               1,
+               1
+             ).data[0] > 0
+           ) {
+             _arr[y][x] = {
+               val: 0,
+               status: "hidden",
+               y: y,
+               x: x,
+               size: size,
+               left: x * size,
+               top: y * size,
+               med: false,
+               updated: 0,
+             };
+           } else {
+             _arr[y][x] = {
+               val: -2,
+               status: "hidden",
+               y: y,
+               x: x,
+               size: size,
+               left: x * size,
+               top: y * size,
+               med: false,
+               updated: 0,
+             };
+           }
+ 
+           //set num
+           context.font = 20 + "px Arial";
+           context.textAlign = "center";
+           context.textBaseline = "middle";
+           context.fillStyle = "gray";
+ 
+           context.fillText(
+             String(_arr[y][x].val),
+             x * size + 20,
+             y * size + 20
+           );
+ 
+           context.strokeStyle = "red";
+           context.strokeRect(
+             x * size + compensation,
+             y * size + compensation,
+             1,
+             1
+           );
+           context.strokeStyle = "green";
+           context.strokeRect(
+             x * size + compensation,
+             y * size + size - compensation,
+             1,
+             1
+           );
+           context.strokeStyle = "blue";
+           context.strokeRect(
+             x * size + size - compensation,
+             y * size + compensation,
+             1,
+             1
+           );
+           context.strokeStyle = "orange";
+           context.strokeRect(
+             x * size + size - compensation,
+             y * size + size - compensation,
+             1,
+             1
+           );
+         }
+       }
+ 
+       // set bombs
+       let b = 0;
+       console.log("spots", this.state.spots);
+       while (b < _spots) {
+         const _y = _random(0, fieldHeight - 1);
+         const _x = _random(0, fieldWidth - 1);
+ 
+         if (_arr[_y][_x].val !== -2 && _arr[_y][_x].val !== -1) {
+           b += 1;
+           _arr[_y][_x].val = -1;
+         }
+       }
+ 
+       // set other blocks
+       for (let y = 0; y < fieldHeight; y++) {
+         for (let x = 0; x < fieldWidth; x++) {
+           if (_arr[y][x].val !== -1 && _arr[y][x].val !== -2) {
+             if (y - 1 >= 0 && x - 1 >= 0) {
+               _arr[y][x].val += +(_arr[y - 1][x - 1].val === -1);
+             }
+ 
+             if (y - 1 >= 0) {
+               _arr[y][x].val += +(_arr[y - 1][x].val === -1);
+             }
+ 
+             if (y - 1 >= 0 && x + 1 < fieldHeight) {
+               _arr[y][x].val += +(_arr[y - 1][x + 1].val === -1);
+             }
+ 
+             if (x + 1 < fieldWidth) {
+               _arr[y][x].val += +(_arr[y][x + 1].val === -1);
+             }
+ 
+             if (y + 1 < fieldHeight && x + 1 < fieldWidth) {
+               _arr[y][x].val += +(_arr[y + 1][x + 1].val === -1);
+             }
+ 
+             if (y + 1 < fieldHeight) {
+               _arr[y][x].val += +(_arr[y + 1][x].val === -1);
+             }
+ 
+             if (y + 1 < fieldHeight && x - 1 >= 0) {
+               _arr[y][x].val += +(_arr[y + 1][x - 1].val === -1);
+             }
+ 
+             if (x - 1 >= 0) {
+               _arr[y][x].val += +(_arr[y][x - 1].val === -1);
+             }
+           }
+         }
+       }
+     });
+
+this.setState({field: _arr, spots: _spots,});
+
+  }
+
+  componentDidUpdate( prevProps,  prevState) {
+    console.log("App componentDidUpdate", prevState.field_size_factor, this.state.field_size_factor, this.state.fieldWidth);
+    if ( prevState.field_size_factor !==  this.state.field_size_factor) {
+
+      this.setNewField();
+
+      
+    }
   }
 
   leftClickHandler = (block) => {
@@ -271,9 +308,7 @@ class App extends Component {
     this.setState({ field: _arr, gameOver: _gameOver });
   };
 
-  componentDidUpdate() {
-    console.log("App componentDidUpdate");
-  }
+
 
   rightClickHandler = (e, block) => {
     e.preventDefault();
@@ -344,7 +379,8 @@ class App extends Component {
       size,
       playGame,
       screenWidth,
-      screenHeight
+      screenHeight,
+      field_size_factor
     } = this.state;
 
     let _style = null;
@@ -363,7 +399,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Header />
+        <Header 
+        field_size_factor =  { field_size_factor }
+        fieldSizeHandler = { this.fieldSizeHandler }
+        />
 
         {playGame === false ? (
           <div className="canvas-block">
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,18 @@
-import './header.style.css'
+import "./header.style.css";
 const Header = (props) => {
-    return (
-        <div><h1 className = "text-white">Header</h1></div>
-    );
-}
+  const  {field_size_factor, fieldSizeHandler } = props;
+  return (
+    <div className="header">
+      <h1 className="text-white">C19 fixer (сапер)</h1> 
+          <select value={field_size_factor} onChange={fieldSizeHandler}>
+            <option value="2">Small</option>
+            <option value="3">Medium</option>
+            <option value="4">Large</option>
+            <option value="5">Super</option>
+          </select>
+       
+    </div>
+  );
+};
 
-export  default Header;
+export default Header;
